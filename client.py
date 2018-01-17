c3bfab9154ec3fac4be9b1bc545173723bf1f304
dan89farhan <dan89farhan@yahoo.com>
2018-01-18 00:28:46 +0530

    Solve networking problem

--- client.py
+++ client.py
@@ -1,22 +1,63 @@
 import socket
+import threading
 
 """This is used to import socket library"""
 
-class Client:
+
+class Client(threading.Thread):
 
     """This is client class to use for client """
 
     def __init__(self):
-
+        """Initialization of method"""
+        threading.Thread.__init__(self)
         self.port = 9090
         self.socket_details = socket.socket()
         self.hostname = socket.gethostname()
+        self.circuit = ''
 
     def connect_to_server(self):
         """This method is use connect to server"""
         print("The value of socket address is ", self.hostname)
         self.socket_details.connect((self.hostname, self.port))
-        print(self.socket_details.recv(1024))
-        self.socket_details.close()
+
+    def receive_message(self):
+        """Receive Message from server endlessly"""
+        while True:
+            message = self.socket_details.recv(1024)
+            print(message)
+            if message == "exit":
+                return    
+        
+
+    def run(self):
+        """Backgound RUN METHOD"""
+        print("Enter 'exit' to stop the client")
+        while True:
+            print("Now you can send message to the server ")
+            print("Enter your message")
+            message = input()
+            if message == "exit":
+                return 0
+            else:
+
+                message = bytes(message, 'utf-8')
+                print(message)
+                encrypt = ''
+                for chars in enumerate(message):
+
+                    encrypt += chr(chars[1] - 3)
+                    
+                
+                encrypt = "[sent from client] > " + encrypt
+                print(encrypt)
+                self.socket_details.send(encrypt.encode())
+
+
 C = Client()
 C.connect_to_server()
+C.start()
+C.receive_message()
+
+C.join()
+print("Client has been shut down")
--- server.py
+++ server.py
@@ -1,41 +1,69 @@
 import socket
+import threading
 
 
-class ServerSocket:
+class ServerSocket(threading.Thread):
     """The main server which runs in background."""
 
     def __init__(self):
-
+        threading.Thread.__init__(self)
         self.port = 9090
-        self.socket_details = socket.socket()
-
+        self.socket_details = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.hostname = socket.gethostname()
-        
+        self.circuit = ""
+        self.add = ""
 
     def create_connection(self):
-
         """For create connection"""
         self.socket_details.bind((self.hostname, self.port))
+        # self.socket_details.connect((self.hostname, self.port))
         self.socket_details.listen(5)
+        
         print(self.socket_details)
 
-    def send_message(self):
-        """for send message"""
+    def receive_message(self):
+        """Receive Message from server endlessly"""
+        while True:
+            try:
+                if self.add != "":
+                    message = self.circuit.recv(1024)
+                    print("[recieved from client] > "+ message)
+                    if message == "exit":
+                        return
+            except ValueError:
+                print("Got an error ", ValueError)    
 
-        circuit, add = self.socket_details.accept()
-        print("Receive new connection from clint ", add)
-        print("Enter your message")
-        message = input()
-        message = bytes(message, 'utf-8')
-        encrypt = ''
-        for i, chars in enumerate(message):
+    def run(self):
+        """Backgound RUN METHOD"""
+        self.circuit, self.add = self.socket_details.accept()
+        print("Receive new connection from clint ", self.add)
+        print("Enter 'exit' to stop the server")
+        while True:
             
-            encrypt += chr(chars+3)
-            print(bytes([chars+3]))
-        circuit.send(encrypt.encode())
-        circuit.close()
+            print("Enter your message")
+            message = input()
+            if message == "exit":
+            
+                return
+            else:
+
+                message = bytes(message, 'utf-8')
+                print(message)
+                encrypt = ''
+                for chars in enumerate(message):
+                    
+                    encrypt += chr(chars[1] + 3)
+                    # print(bytes([chars[1] + 3]))
+                encrypt = "[sent from server] > " + encrypt
+                print(encrypt)
+                self.circuit.send(encrypt.encode())
 
 
 S = ServerSocket()
+
 S.create_connection()
-S.send_message()
+S.start()
+S.receive_message()
+S.join()
+print("Server has beed shut down.")
+S.circuit.close()
